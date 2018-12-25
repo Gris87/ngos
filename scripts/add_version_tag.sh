032b37cd3d34cdc9f52f08f62f73d78acf88de88
Gris87 <Gris87@yandex.ru>
2018-12-25 15:02:12 +0300

    More scripts

---
+++ scripts/add_version_tag.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This script helps to add tag for new version
+# Author: Maxim Shvecov
+# Usage: ./add_version_tag.sh
+
+
+
+###########################################################################################
+#    PROCESSING
+###########################################################################################
+
+
+
+if [ ! -f ngos.files ]; then
+    echo "Please run this script from the root folder"
+    exit 1
+fi
+
+
+
+LAST_VERSION=`git tag | grep -E -e "[0-9]+_v[0-9]+\.[0-9]+\.[0-9]+" | sort | tail -n 1 | cut -d _ -f 2 | cut -c 2-`
+
+
+
+IS_RC=`echo "${LAST_VERSION}" | grep rc`
+
+if [ "${IS_RC}" != "" ]; then
+    NEW_VERSION=`echo "${LAST_VERSION}" | cut -d "-" -f 1`
+else
+    VERSION_PARTS=(`echo "${LAST_VERSION}" | tr "\." " "`)
+
+
+
+    if [ ${VERSION_PARTS[2]} -ge 9 ]; then
+        VERSION_PARTS[2]=0
+        VERSION_PARTS[1]=$((${VERSION_PARTS[1]} + 1))
+    else
+        VERSION_PARTS[2]=$((${VERSION_PARTS[2]} + 1))
+    fi
+
+
+
+    NEW_VERSION=${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.${VERSION_PARTS[2]}
+fi
+
+
+
+NEW_TAG=`date +%Y%m%d`_v${NEW_VERSION}
+
+
+
+git tag -a ${NEW_TAG} -m "Version ${NEW_VERSION}"
+./scripts/commit.sh "Version ${NEW_VERSION}"
+
+
+
+exit 0
