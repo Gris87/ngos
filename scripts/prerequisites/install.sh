b319592b992a698bb0f262ec521c77de83be5612
Gris87 <Gris87@yandex.ru>
2019-01-09 19:25:28 +0300

    Small fix

--- scripts/prerequisites/install.sh
+++ scripts/prerequisites/install.sh
@@ -16,6 +16,14 @@ if [[ "`lsb_release -rs`" == "18.04" ]]; then
     ./ubuntu_18_04.sh
 elif [[ "`lsb_release -rs`" == "16.04" ]]; then
     ./ubuntu_16_04.sh
-else
+elif [[ "`lsb_release -rs`" == "7" ]]; then
     ./centos_7.sh
+else
+    echo "Unknown OS"
+
+    exit 1
 fi
+
+
+
+exit 0
