9940d1fec479d4c0f6ce659dc842be6f48187084
Ubuntu <shvetsov@cps-etl-srv.zkwrtzfq2pmute0qde2p35n2sd.fx.internal.cloudapp.net>
2019-01-28 13:17:57 +0000

    Refactoring

--- tools/tracers/linux.sh
+++ tools/tracers/linux.sh
@@ -28,7 +28,7 @@ VM_LINUX_ELF=../../../${OS_NAME}/vmlinux
 INITIAL_BREAKPOINT=0x3FFA3343
 INITIAL_ADDRESS=0x39E6B4F0
 PREFERRED_ADDRESS=0x1400000
-RELOCATED_ADDRESS=0x2CDB000
+RELOCATED_ADDRESS=0x35214B0
 RANDOM_PHYSICAL_ADDRESS=0x3600000
 RANDOM_VIRTUAL_ADDRESS=0xFFFFFFFF81000000
 
@@ -217,14 +217,19 @@ echo ""
 
 
 
+EFI_PE_ENTRY_OFFSET=`readelf -s ${BZ_IMAGE_ELF} | grep efi_pe_entry | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | cut -d " " -f 2`
+RELOCATED_OFFSET=`readelf    -s ${BZ_IMAGE_ELF} | grep relocated    | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | cut -d " " -f 2`
+
+
+
 sleep 5
 execute_gdb_command "set width 200" -v
 execute_gdb_command "set listsize 1" -v
 execute_gdb_command "set architecture i386:x86-64:intel" -v
 execute_gdb_command "target remote :1234" -v
-execute_gdb_command "add-symbol-file-all ${BZ_IMAGE_ELF} ${INITIAL_ADDRESS}-0x2F0" -v
+execute_gdb_command "add-symbol-file-all ${BZ_IMAGE_ELF} ${INITIAL_ADDRESS}-0x${EFI_PE_ENTRY_OFFSET}" -v
 execute_gdb_command "add-symbol-file-all ${BZ_IMAGE_ELF} ${PREFERRED_ADDRESS}" -v
-execute_gdb_command "add-symbol-file-all ${BZ_IMAGE_ELF} ${RELOCATED_ADDRESS}" -v
+execute_gdb_command "add-symbol-file-all ${BZ_IMAGE_ELF} ${RELOCATED_ADDRESS}-0x${RELOCATED_OFFSET}" -v
 execute_gdb_command "add-symbol-file-all ${VM_LINUX_ELF} -0xFFFFFFFF81000000+${RANDOM_PHYSICAL_ADDRESS}" -v
 execute_gdb_command "add-symbol-file-all ${VM_LINUX_ELF} -0xFFFFFFFF81000000+${RANDOM_VIRTUAL_ADDRESS}" -v
 
