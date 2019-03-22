eab5b24908d6da25b09223d3481f3a205894fd9e
Gris87 <Gris87@yandex.ru>
2019-03-22 13:49:35 +0300

    webapps

---
+++ webapps/ngos/html/common/constants.php
@@ -0,0 +1,10 @@
+<?php
+    $DB_HOST     = "localhost";
+    $DB_USER     = "ngos";
+    $DB_PASSWORD = "ngos";
+    $DB_NAME     = "ngos";
+
+    $DB_TABLE_APPS       = "apps";
+    $DB_TABLE_PROPERTIES = "properties";
+    $DB_TABLE_SERVERS    = "servers";
+?>
---
+++ webapps/ngos/html/common/functions.php
@@ -0,0 +1,26 @@
+<?php
+require_once "constants.php";
+
+
+
+function db_connect()
+{
+    $link = new mysqli($GLOBALS["DB_HOST"], $GLOBALS["DB_USER"], $GLOBALS["DB_PASSWORD"], $GLOBALS["DB_NAME"]);
+
+    if ($link->connect_error)
+    {
+        error_log("Failed to connect to MariaDB server. Error: " . $link->connect_error);
+
+        return null;
+    }
+
+    return $link;
+}
+
+
+
+function db_disconnect($link)
+{
+    $link->close();
+}
+?>
