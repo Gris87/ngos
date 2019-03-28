4a50d9f675ab499d5a809f8c2d276b1cb6ba89fb
Gris87 <Gris87@yandex.ru>
2019-03-28 09:02:03 +0000

    webapps

--- webapps/ngos/install.sh
+++ webapps/ngos/install.sh
@@ -47,6 +47,10 @@ fi
 
 
 
+cp -r html/. /var/www/html
+
+
+
 mysql -u root < mariadb/create.sql
 
 
@@ -58,14 +62,25 @@ mysql -u root -D ngos -e "INSERT IGNORE INTO regions (id, name) VALUES ('1', 'Ir
 FIRST_SERVER=`mysql -u root -D ngos -NB -e "SELECT id FROM servers WHERE address='${FIRST_SERVER_ADDRESS}';"`
 
 if [ "${FIRST_SERVER}" == "" ]; then
-    curl -k https://${FIRST_SERVER_ADDRESS}/rest/ping.php
+    PING_TOTAL=0
 
-    mysql -u root -D ngos -e "INSERT INTO servers (region_id, address, delay) VALUES ('1', '${FIRST_SERVER_ADDRESS}', '0');"
+    for ((i = 0; i < 10; i++))
+    do
+        PING_RESPONSE=`curl -k -s -w "%{time_total}" https://${FIRST_SERVER_ADDRESS}/rest/ping.php`
+
+        if [ "${PING_RESPONSE:0:15}" != "{\"status\":\"OK\"}" ]; then
+            echo "Failed to ping server: ${FIRST_SERVER_ADDRESS}"
+
+            exit 1
+        fi
+
+        PING_TOTAL=`echo "${PING_TOTAL} + ${PING_RESPONSE:15}" | bc`
+    done
+
+    DELAY=`echo "${PING_TOTAL} * 100000 / 1" | bc`
+
+    mysql -u root -D ngos -e "INSERT INTO servers (region_id, address, delay) VALUES ('1', '${FIRST_SERVER_ADDRESS}', '${DELAY}');"
 fi
-
-
-
-cp -r html/. /var/www/html
 
 
 
