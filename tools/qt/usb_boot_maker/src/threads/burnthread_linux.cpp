d4789286b0627ec96a0b2fd0737a4133ab46bb3a
Gris87 <Gris87@yandex.ru>
2019-05-16 11:22:03 +0300

    USB Boot Maker in Linux

--- tools/qt/usb_boot_maker/src/threads/burnthread_linux.cpp
+++ tools/qt/usb_boot_maker/src/threads/burnthread_linux.cpp
@@ -10,6 +10,11 @@
 #include <QDebug>
 #include <QProcess>
 #include <QRegularExpression>
+#include <QTemporaryFile>
+
+
+
+#define MBR_RESERVED 0x01B8
 
 
 
@@ -47,6 +52,44 @@ void notifyNextStep(BurnThread *thread)
     thread->notifyProgress(currentStep, 6);
 }
 
+void execWithSu(BurnThread *thread, QProcess *suProcess, QString command)
+{
+    Q_ASSERT(thread);
+    Q_ASSERT(suProcess);
+
+
+
+    QTemporaryFile tempFile;
+
+    tempFile.open();
+    QString tempFilePath = tempFile.fileName();
+    tempFile.close();
+
+
+
+    suProcess->write(QString(command + " > " + tempFilePath + "\n").toUtf8());
+    suProcess->write("echo \">>>>>> END <<<<<<\"\n");
+    suProcess->waitForBytesWritten(-1);
+    suProcess->waitForReadyRead(-1);
+
+    while (thread->isWorking())
+    {
+        QString line = suProcess->readLine().trimmed();
+
+        if (line == ">>>>>> END <<<<<<")
+        {
+            break;
+        }
+    }
+
+
+
+    tempFile.open();
+    qDebug() << "";
+    qDebug() << tempFile.readAll().data();
+    tempFile.close();
+}
+
 void unmountVolumes(BurnThread *thread, QProcess *suProcess)
 {
     Q_ASSERT(thread);
@@ -75,20 +118,7 @@ void unmountVolumes(BurnThread *thread, QProcess *suProcess)
 
 
 
-            suProcess->write(QString("umount " + mountPoint + "\n").toUtf8());
-            suProcess->write("echo \">>>>>> END <<<<<<\"\n");
-            suProcess->waitForBytesWritten(-1);
-            suProcess->waitForReadyRead(-1);
-
-            while (thread->isWorking())
-            {
-                QString line = suProcess->readLine().trimmed();
-
-                if (line == ">>>>>> END <<<<<<")
-                {
-                    break;
-                }
-            }
+            execWithSu(thread, suProcess, "umount " + mountPoint);
 
             found = true;
         }
@@ -110,10 +140,7 @@ void clearGpt(BurnThread *thread, QProcess *suProcess)
 
 
 
-    suProcess->write(QString("sgdisk --zap-all /dev/" + thread->getSelectedUsb().deviceName + " &\n").toUtf8());
-    suProcess->write("wait $!\n");
-    suProcess->waitForBytesWritten(-1);
-    suProcess->waitForReadyRead(-1);
+    execWithSu(thread, suProcess, "sgdisk --zap-all /dev/" + thread->getSelectedUsb().deviceName);
 }
 
 void createPartition(BurnThread *thread, QProcess *suProcess)
@@ -126,28 +153,82 @@ void createPartition(BurnThread *thread, QProcess *suProcess)
 
 
 
-    suProcess->write(QString("sgdisk --new 1::-0 --typecode=1:EF00 --change-name=1:\"EFI System\" /dev/" + thread->getSelectedUsb().deviceName + " &\n").toUtf8());
-    suProcess->write("wait $!\n");
-    suProcess->waitForBytesWritten(-1);
-    suProcess->waitForReadyRead(-1);
+    execWithSu(thread, suProcess, "sgdisk --new 1::-0 --typecode=1:EF00 --change-name=1:\"EFI System\" /dev/" + thread->getSelectedUsb().deviceName);
 }
 
-void formatPartition(BurnThread *thread)
+void formatPartition(BurnThread *thread, QProcess *suProcess)
 {
     Q_ASSERT(thread);
 
 
 
     thread->addLog(QCoreApplication::translate("BurnThread", "Formatting partition to FAT32"));
+
+
+
+    execWithSu(thread, suProcess, "mkfs.fat -F32 -s2 /dev/" + thread->getSelectedUsb().deviceName + "1");
 }
 
-void writeProtectiveMbr(BurnThread *thread)
+void writeProtectiveMbr(BurnThread *thread, QProcess *suProcess)
 {
     Q_ASSERT(thread);
 
 
 
     thread->addLog(QCoreApplication::translate("BurnThread", "Writing protective MBR"));
+
+
+
+    QTemporaryFile tempFile;
+
+    tempFile.open();
+    QString tempFilePath = tempFile.fileName();
+    tempFile.close();
+
+
+
+    execWithSu(thread, suProcess, "dd if=/dev/" + thread->getSelectedUsb().deviceName + " of=" + tempFilePath + " bs=512 count=1");
+
+
+
+    tempFile.open();
+    QByteArray originalBuffer = tempFile.readAll();
+    tempFile.close();
+
+
+
+    if (originalBuffer.size() == 512)
+    {
+        QFile file(":/assets/binaries/protective_mbr.bin");
+
+        if (!file.open(QIODevice::ReadOnly))
+        {
+            qCritical() << "Failed to open :/assets/binaries/protective_mbr.bin";
+
+            thread->stop();
+
+            return;
+        }
+
+        QByteArray buffer = file.readAll();
+
+        file.close();
+
+
+
+        buffer.replace(MBR_RESERVED, 512 - MBR_RESERVED, originalBuffer.mid(MBR_RESERVED));
+        buffer.replace(510, 2, "\x55\xAA");
+
+
+
+        tempFile.open();
+        tempFile.write(buffer);
+        tempFile.close();
+
+
+
+        execWithSu(thread, suProcess, "dd if=" + tempFilePath + " of=/dev/" + thread->getSelectedUsb().deviceName);
+    }
 }
 
 void formatDisk(BurnThread *thread, QProcess *suProcess)
@@ -164,11 +245,11 @@ void formatDisk(BurnThread *thread, QProcess *suProcess)
     notifyNextStep(thread);
     CHECK_IF_THREAD_TERMINATED(thread);
 
-    formatPartition(thread);
+    formatPartition(thread, suProcess);
     notifyNextStep(thread);
     CHECK_IF_THREAD_TERMINATED(thread);
 
-    writeProtectiveMbr(thread);
+    writeProtectiveMbr(thread, suProcess);
     notifyNextStep(thread);
     CHECK_IF_THREAD_TERMINATED(thread);
 }
