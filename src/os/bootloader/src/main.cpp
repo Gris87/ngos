1e6852d11c9904dcb65c72e133d0a4bb957cfe98
Gris87 <Gris87@yandex.ru>
2019-06-18 11:57:45 +0000

    Small fix

--- src/os/bootloader/src/main.cpp
+++ src/os/bootloader/src/main.cpp
@@ -1,3 +1,8 @@
+#include <ngos/linkage.h>
+#include <uefi/uefisystemtable.h>
+
+
+
 // DO NOT DELETE IT!!!
 // We are putting this variable to .noinit section in order to convert .noinit section type from NOBITS to PROGBITS
 u8 __reserved_for_bss __attribute__ ((section (".noinit")));
@@ -5,7 +10,7 @@ u8 __reserved_for_bss __attribute__ ((section (".noinit")));
 
 
 CPP_EXTERN_C
-int uefiMain(uefi_handle imageHandle, UefiSystemTable *systemTable, u64 kernelLocation)
+i64 uefiMain(uefi_handle /*imageHandle*/, UefiSystemTable * /*systemTable*/, u64 /*kernelLocation*/)
 {
     return 0;
 }
--- src/os/bootloader/test/bits64/a_uefi/testresults.h
+++ src/os/bootloader/test/bits64/a_uefi/testresults.h
@@ -1,36 +1,6 @@
-#ifndef CONFIGURE_TEST_BITS64_A_UEFI_TESTRESULTS_H
-#define CONFIGURE_TEST_BITS64_A_UEFI_TESTRESULTS_H
+#ifndef BOOTLOADER_TEST_BITS64_A_UEFI_TESTRESULTS_H
+#define BOOTLOADER_TEST_BITS64_A_UEFI_TESTRESULTS_H
 
 
 
-#include <buildconfig.h>
-#include <ngos/status.h>
-
-
-
-#if NGOS_BUILD_TEST_MODE == OPTION_YES
-
-
-
-class TestResults
-{
-public:
-    TestResults();
-
-    void testPassed();
-    void testFailed(const char *filename, u64 line, const char *description);
-
-    NgosStatus summary();
-
-private:
-    u64 mPassed;
-    u64 mFailed;
-};
-
-
-
-#endif
-
-
-
-#endif // CONFIGURE_TEST_BITS64_A_UEFI_TESTRESULTS_H
+#endif // BOOTLOADER_TEST_BITS64_A_UEFI_TESTRESULTS_H
