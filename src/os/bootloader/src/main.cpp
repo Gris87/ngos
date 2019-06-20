ebfdc412de98998325e87ca6773fa7ef08cb81d5
Gris87 <Gris87@yandex.ru>
2019-06-20 11:04:08 +0000

    Bootloader

--- src/os/bootloader/src/main.cpp
+++ src/os/bootloader/src/main.cpp
@@ -1,6 +1,14 @@
+#include <bootparams/bootparams.h>
+#include <buildconfig.h>
+#include <common/src/bits64/assets/assets.h>
+#include <common/src/bits64/cpu/cpu.h>
+#include <common/src/bits64/fpu/fpu.h>
+#include <common/src/bits64/serial/serial.h>
 #include <ngos/linkage.h>
-#include <uefi/uefistatus.h>
 #include <uefi/uefisystemtable.h>
+#include <uefibase/src/bits64/main/setupgraphics.h>
+#include <uefibase/src/bits64/uefi/uefiassert.h>
+#include <uefibase/src/bits64/uefi/uefilog.h>
 #include <uefibase/test/bits64/sections/section0/testcase.h>
 
 
@@ -11,8 +19,93 @@ u8 __reserved_for_bss __attribute__ ((section (".noinit")));
 
 
 
+#if NGOS_BUILD_UEFI_LOG_LEVEL == OPTION_LOG_LEVEL_INHERIT && NGOS_BUILD_LOG_LEVEL >= OPTION_LOG_LEVEL_VERBOSE || NGOS_BUILD_UEFI_LOG_LEVEL >= OPTION_LOG_LEVEL_VERBOSE
+NgosStatus printCpuFlags()
+{ // Ignore CppNgosTraceVerifier
+    UEFI_LT((""));
+
+
+
+    char buffer[1024];
+
+    UEFI_ASSERT_EXECUTION(CPU::flagsToString(buffer, 1024), NgosStatus::ASSERTION);
+
+    UEFI_LV(("CPU flags:"));
+    UEFI_LV(("%s", buffer));
+
+
+
+    return NgosStatus::OK;
+}
+#endif
+
 CPP_EXTERN_C
-UefiStatus uefiMain(uefi_handle /*imageHandle*/, UefiSystemTable * /*systemTable*/, u64 /*kernelLocation*/)
-{
+UefiStatus uefiMain(uefi_handle imageHandle, UefiSystemTable *systemTable, u64 kernelLocation)
+{ // Ignore CppNgosTraceVerifier
+    // We can't output at the moment
+    // UEFI_LT((" | imageHandle = 0x%p, systemTable = 0x%p, kernelLocation = 0x%p", imageHandle, systemTable, kernelLocation)); // Commented to avoid error because UEFI is uninitialized
+
+
+
+    UEFI_ASSERT_EXECUTION(Serial::initConsole(), UefiStatus::ABORTED);
+
+
+
+    // Check that we are booting via UEFI
+    if (systemTable->header.signature != UEFI_SYSTEM_TABLE_SIGNATURE)
+    {
+        UEFI_LF(("Unexpected UEFI System Table signature"));
+
+        return UefiStatus::ABORTED;
+    }
+
+
+
+    UEFI_ASSERT_EXECUTION(UEFI::init(imageHandle, systemTable), UefiStatus::ABORTED);
+
+
+
+    UEFI_LT((" | imageHandle = 0x%p, systemTable = 0x%p, kernelLocation = 0x%p", imageHandle, systemTable, kernelLocation)); // We can output now
+
+    UEFI_ASSERT(imageHandle,    "imageHandle is null",    UefiStatus::ABORTED);
+    UEFI_ASSERT(systemTable,    "systemTable is null",    UefiStatus::ABORTED);
+    UEFI_ASSERT(kernelLocation, "kernelLocation is null", UefiStatus::ABORTED);
+
+
+
+    UEFI_LI(("NGOS Bootloader starting up"));
+    UEFI_LV(("Kernel started at address 0x%p", kernelLocation));
+
+
+
+    UEFI_ASSERT_EXECUTION(CPU::init(), UefiStatus::ABORTED);
+    UEFI_LI(("CPU information initialized"));
+
+#if NGOS_BUILD_UEFI_LOG_LEVEL == OPTION_LOG_LEVEL_INHERIT && NGOS_BUILD_LOG_LEVEL >= OPTION_LOG_LEVEL_VERBOSE || NGOS_BUILD_UEFI_LOG_LEVEL >= OPTION_LOG_LEVEL_VERBOSE
+    UEFI_ASSERT_EXECUTION(printCpuFlags(), UefiStatus::ABORTED);
+#endif
+
+
+
+    UEFI_ASSERT_EXECUTION(FPU::init(), UefiStatus::ABORTED);
+
+
+
+    UEFI_ASSERT_EXECUTION(Assets::init(), UefiStatus::ABORTED);
+    UEFI_LI(("Assets initialized"));
+
+
+
+#if NGOS_BUILD_TEST_MODE == OPTION_YES
+    if (startTestSection0() != NgosStatus::OK)
+    {
+        UEFI_LF(("Test failure"));
+
+        return UefiStatus::ABORTED;
+    }
+#endif
+
+
+
     return UefiStatus::SUCCESS;
 }
