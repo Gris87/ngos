c0737ca417ede845c6a8ada62c320dc303fc91ab
Gris87 <Gris87@yandex.ru>
2019-08-02 20:01:27 +0000

    QMake

--- tools/qt/qmake/src/main/qmake.h
+++ tools/qt/qmake/src/main/qmake.h
@@ -15,19 +15,21 @@ public:
     qint64 process(); // TEST: NO
 
 private:
-    qint64 processInWorkingDirectory(const QString &workingDirectory, const QString &pathToProFile); // TEST: NO
+    qint64 processInWorkingDirectory(const QString &workingDirectory, const QString &proFilename); // TEST: NO
     qint64 processLines(const QString &workingDirectory, const QStringList &lines); // TEST: NO
     bool parseEntry(const QString &workingDirectory, const QString &entryName, const QString &entryOperator, const QString &entryValue); // TEST: NO
     qint64 generateMakefile(const QString &workingDirectory); // TEST: NO
     qint64 generateSubdirsMakefile(const QString &workingDirectory); // TEST: NO
     qint64 generateApplicationMakefile(const QString &workingDirectory); // TEST: NO
     qint64 generateLibraryMakefile(const QString &workingDirectory); // TEST: NO
+    qint64 save(const QString &workingDirectory, const QStringList &lines);
 
     QString                    mPathToProFile;
     QRegularExpression         mCommentRegexp;
     QRegularExpression         mEntryRegexp;
     QRegularExpression         mEntryValueRegexp;
     QMap<QString, QStringList> mEntries;
+    QStringList                mMakefileDependencies;
 };
 
 
