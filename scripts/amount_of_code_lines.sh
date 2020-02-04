6c68c448fa01c10d0c6726ed32212ad6bc741c8f
Gris87 <Gris87@yandex.ru>
2020-02-04 15:19:37 +0000

    Calculate amount of lines

---
+++ scripts/amount_of_code_lines.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# This script helps to calculate amount of code lines for the whole project except generated files
+# Author: Maxim Shvecov
+# Usage: ./scripts/amount_of_code_lines.sh
+
+
+
+###########################################################################################
+#    PROCESSING
+###########################################################################################
+
+
+
+if [ ! -f ngos.files ]; then
+    echo "Please run this script from the root folder"
+    exit 1
+fi
+
+
+
+OLD_IFS=${IFS}
+IFS=$'\n'
+
+wc -l ` \
+        cat ngos.files | \
+            grep \
+                -ve tools/tracers/assets/ \
+                -ve \.bin$ \
+                -ve \.dll$ \
+                -ve \.exe$ \
+                -ve \.ttf$ \
+                -ve \.ico$ \
+                -ve \.png$ \
+                -ve \.jpg$ \
+                -ve \.gif$ \
+                -ve \.svg$ \
+    ` | \
+    tail -n 1 | \
+    cut -d " " -f 2
+
+IFS=${OLD_IFS}
+
+
+
+exit 0
