0cd89e3a9e15726e87222e5d486091c932a8148a
Gris87 <Gris87@yandex.ru>
2020-02-12 11:47:26 +0000

    Secure Boot

--- scripts/extract_secure_boot_private_key.sh
+++ scripts/extract_secure_boot_private_key.sh
@@ -7,14 +7,29 @@
 
 
 ###########################################################################################
-#    PROCESSING
+#    VERIFICATION
 ###########################################################################################
 
 
 
 if [ ! -f ngos.files ]; then
     echo "Please run this script from the root folder"
+
     exit 1
+fi
+
+
+
+###########################################################################################
+#    PROCESSING
+###########################################################################################
+
+
+
+if [ ! -f secure_boot/keys/ISK.key ]; then
+    gpg -d -o secure_boot/keys/ISK.key secure_boot/keys/ISK.key.gpg
+else
+    echo "Secure boot private key already extracted"
 fi
 
 
--- scripts/sign_uefi_application.sh
+++ scripts/sign_uefi_application.sh
@@ -2,7 +2,39 @@
 
 # This script helps to sign UEFI applications with the keys for Secure Boot
 # Author: Maxim Shvecov
-# Usage: ./scripts/sign_uefi_application.sh
+# Usage: ./scripts/sign_uefi_application.sh APPLICATION_PATH
+
+
+
+###########################################################################################
+#    PARAMETERS
+###########################################################################################
+
+
+
+APPLICATION_PATH=$1
+
+
+
+###########################################################################################
+#    VERIFICATION
+###########################################################################################
+
+
+
+if [ ! -f ngos.files ]; then
+    echo "Please run this script from the root folder"
+
+    exit 1
+fi
+
+
+
+if [ "${APPLICATION_PATH}" == "" ]; then
+    echo "Usage: ./scripts/sign_uefi_application.sh APPLICATION_PATH"
+
+    exit 1
+fi
 
 
 
@@ -12,9 +44,10 @@
 
 
 
-if [ ! -f ngos.files ]; then
-    echo "Please run this script from the root folder"
-    exit 1
+if [ -f secure_boot/keys/ISK.key ]; then
+    sbsign --key secure_boot/keys/ISK.key --cert secure_boot/keys/ISK.pem --output ${APPLICATION_PATH} ${APPLICATION_PATH}
+else
+    echo "Secure boot private key is unavailable. Please run ./scripts/extract_secure_boot_private_key.sh" 1>&2
 fi
 
 
