28d905a004523ed37e138062c3b68c6d341fd29e
Gris87 <Gris87@yandex.ru>
2020-07-24 16:04:26 +0300

    Small fix

--- scripts/prerequisites/ubuntu_16_04.sh
+++ scripts/prerequisites/ubuntu_16_04.sh
@@ -15,12 +15,6 @@
 CURRENT_PATH=`pwd`
 BINUTILS_VERSION=2.34
 GCC_VERSION=10.1.0
-LIBVIRT_VERSION=6.5.0
-LIBVIRT_GLIB_VERSION=3.0.0
-QEMU_VERSION=5.0.0
-VIRT_MANAGER_VERSION=2.2.1
-VIRT_VIEWER_VERSION=9.0
-OVMF_VERSION=edk2-stable202005
 VIRTUALBOX_VERSION=6.0
 QT_VERSION=5.15.0
 
@@ -73,28 +67,10 @@ echo ""
 mkdir /tmp/src/
 cd /tmp/src/
 
-sed -i "s/^# deb-src /deb-src /" /etc/apt/sources.list
 apt-get update
 apt-get upgrade -y
-apt-get install -y curl
 apt-get install -y build-essential
-apt-get install -y libncurses-dev
-apt-get install -y libelf-dev
-apt-get install -y bison
-apt-get install -y flex
-apt-get install -y gdb
-apt-get install -y libgl-dev
-apt-get install -y socat
-apt-get install -y imagemagick
-apt-get install -y npm
-apt-get install -y efitools
-apt-get install -y sbsigntool
-apt-get install -y cmake
 apt-get install -y texinfo
-
-ln -s /usr/bin/nodejs /usr/bin/node
-npm i sinon --save-dev
-npm i markdown-spellcheck -g
 
 export PREFIX="/usr/local/x8664elfgcc"
 export TARGET=x86_64-elf
@@ -103,12 +79,12 @@ export PATH="${PREFIX}/bin:${PATH}"
 
 
 echo ""
-echo -e "\e[33m-------------------- gcc-9.3 --------------------\e[0m"
+echo -e "\e[33m-------------------- gcc-9 --------------------\e[0m"
 echo ""
 
 
 
-add-apt-repository -y ppa:jonathonf/gcc-9.3
+add-apt-repository -y ppa:jonathonf/gcc
 apt-get update
 apt-get install -y gcc-9 g++-9
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90 --slave /usr/bin/g++ g++ /usr/bin/g++-9
@@ -166,196 +142,15 @@ make install-target-libgcc                                                      
 
 
 echo ""
-echo -e "\e[33m-------------------- libvirt-${LIBVIRT_VERSION} --------------------\e[0m"
+echo -e "\e[33m-------------------- libvirt --------------------\e[0m"
 echo ""
 
 
 
-apt-get build-dep -y libvirt
-apt install -y python-pip
-pip install rst2html5
-
-
-
-mkdir /tmp/src
-cd /tmp/src
-
-if [ ! -d libvirt ]; then
-    git clone https://github.com/libvirt/libvirt.git
-fi
-
-cd libvirt
-git checkout master
-git reset --hard
-git clean -df
-git pull
-git reset --hard v${LIBVIRT_VERSION}
-cd ..
-
-mkdir libvirt-build
-cd libvirt-build
-../libvirt/autogen.sh --system || exit 1
-make -j`nproc` all             || exit 1
-make install                   || exit 1
+apt-get install -y qemu qemu-kvm libvirt-daemon bridge-utils virt-manager virtinst
 
 systemctl enable libvirtd
 systemctl restart libvirtd
-
-
-
-echo ""
-echo -e "\e[33m-------------------- libvirt-glib-${LIBVIRT_GLIB_VERSION} --------------------\e[0m"
-echo ""
-
-
-
-apt-get build-dep -y libvirt-glib
-apt-get install -y gtk-doc-tools
-
-
-
-mkdir /tmp/src
-cd /tmp/src
-
-if [ ! -d libvirt-glib ]; then
-    git clone https://github.com/libvirt/libvirt-glib.git
-fi
-
-cd libvirt-glib
-git checkout master
-git reset --hard
-git clean -df
-git pull
-git reset --hard v${LIBVIRT_GLIB_VERSION}
-cd ..
-
-mkdir libvirt-glib-build
-cd libvirt-glib-build
-../libvirt-glib/autogen.sh --system || exit 1
-make -j`nproc` all                  || exit 1
-make install                        || exit 1
-
-
-
-echo ""
-echo -e "\e[33m-------------------- qemu-${QEMU_VERSION} --------------------\e[0m"
-echo ""
-
-
-
-apt-get install -y libpixman-1-dev
-
-
-
-mkdir /tmp/src
-cd /tmp/src
-
-if [ ! -d qemu ]; then
-    git clone https://github.com/qemu/qemu.git
-fi
-
-cd qemu
-git checkout master
-git reset --hard
-git clean -df
-git pull
-git reset --hard v${QEMU_VERSION}
-cd ..
-
-mkdir qemu-build
-cd qemu-build
-../qemu/configure --enable-debug || exit 1
-make -j`nproc` all               || exit 1
-make install                     || exit 1
-
-
-
-echo ""
-echo -e "\e[33m-------------------- virt-manager-${VIRT_MANAGER_VERSION} --------------------\e[0m"
-echo ""
-
-
-
-apt-get install -y intltool libgtk2.0-bin python3-pip libosinfo-1.0
-pip3 install libvirt-python
-pip3 install libxml2-python3
-
-
-
-mkdir /tmp/src
-cd /tmp/src
-
-if [ ! -d virt-manager-${VIRT_MANAGER_VERSION} ]; then
-    wget https://virt-manager.org/download/sources/virt-manager/virt-manager-${VIRT_MANAGER_VERSION}.tar.gz
-    tar xf virt-manager-${VIRT_MANAGER_VERSION}.tar.gz
-fi
-
-cd virt-manager-${VIRT_MANAGER_VERSION}/
-./setup.py install 2>&1 || exit 1
-
-
-
-echo ""
-echo -e "\e[33m-------------------- virt-viewer-${VIRT_VIEWER_VERSION} --------------------\e[0m"
-echo ""
-
-
-
-apt-get install -y libgtk-vnc-2.0-dev
-
-
-
-mkdir /tmp/src
-cd /tmp/src
-
-if [ ! -d virt-viewer-${VIRT_VIEWER_VERSION} ]; then
-    wget https://virt-manager.org/download/sources/virt-viewer/virt-viewer-${VIRT_VIEWER_VERSION}.tar.gz
-    tar xf virt-viewer-${VIRT_VIEWER_VERSION}.tar.gz
-fi
-
-mkdir virt-viewer-build
-cd virt-viewer-build
-../virt-viewer-${VIRT_VIEWER_VERSION}/configure || exit 1
-make -j`nproc` all                              || exit 1
-make install                                    || exit 1
-
-
-
-rm /usr/lib/x86_64-linux-gnu/libvirt*
-systemctl restart libvirtd
-
-
-
-echo ""
-echo -e "\e[33m-------------------- OVMF-${OVMF_VERSION} --------------------\e[0m"
-echo ""
-
-
-
-apt-get install -y nasm iasl
-
-
-
-cd /usr/local/
-
-if [ ! -d edk2 ]; then
-    git clone https://github.com/tianocore/edk2.git
-    cd edk2
-    git submodule update --init
-    cd ..
-fi
-
-cd edk2
-git checkout master
-git reset --hard
-git clean -df
-git pull
-git reset --hard ${OVMF_VERSION}
-git submodule update
-
-source edksetup.sh BaseTools                                       || exit 1
-make -C BaseTools/                                                 || exit 1
-build -a X64 -t GCC5 -b DEBUG -n `nproc` -p OvmfPkg/OvmfPkgX64.dsc || exit 1
 
 
 
@@ -420,21 +215,5 @@ echo -e "\e[31m* Qt -> Qt ${QT_VERSION} -> Sources\e[0m"
 
 
 echo ""
-echo -e "\e[33m-------------------- AppArmor --------------------\e[0m"
-echo ""
-
-
-
-update-rc.d apparmor disable
-
-
-
-echo ""
 echo -e "\e[32m-------------------- Done --------------------\e[0m"
 echo ""
-
-
-
-echo "AppArmor disabled. Rebooting..."
-sleep 5
-reboot
