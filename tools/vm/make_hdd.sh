37e06d1fb480dc3ad37088a513df06310d7149d5
Gris87 <Gris87@yandex.ru>
2021-06-09 13:53:51 +0000

    Refactoring

--- tools/vm/make_hdd.sh
+++ tools/vm/make_hdd.sh
@@ -71,7 +71,7 @@ mkdir -p ../../build/disks/ 2> /dev/null                                        
                                                                                                                                                                                                          # Colorize: green
                                                                                                                                                                                                          # Colorize: green
                                                                                                                                                                                                          # Colorize: green
-truncate --size ${HDD_SIZE} "../../build/disks/${VM_NAME}.img"                                                                                                                                           # Colorize: green
+truncate --size ${HDD_SIZE}G "../../build/disks/${VM_NAME}.img"                                                                                                                                           # Colorize: green
 sgdisk --zap-all \
     --new 1::+100M --typecode=1:EF00 --change-name=1:"UEFI System" \
     --new 2::-0 --typecode=2:8300 --change-name=2:"NGOS filesystem" \
