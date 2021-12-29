8c12af34d6251d610b5505db1bf15218ef39f161
Gris87 <Gris87@yandex.ru>
2021-12-29 20:46:49 +0000

    PeSpecVerifier

--- tools/qt/image_builder/src/com/ngos/devtools/image_builder/other/coffheadermachine.h
+++ tools/qt/image_builder/src/com/ngos/devtools/image_builder/other/coffheadermachine.h
@@ -11,36 +11,42 @@
                                                                                                                                                                                                          // Colorize: green
                                                                                                                                                                                                          // Colorize: green
 enum class COFFHeaderMachine: quint16                                                                                                                                                                    // Colorize: green
-{                                                                                                                                                                                                        // Colorize: green
-    INTEL_386                           = 0x014C,                                                                                                                                                        // Colorize: green
-    X64                                 = 0x8664,                                                                                                                                                        // Colorize: green
-    MIPS_R3000                          = 0x0162,                                                                                                                                                        // Colorize: green
-    MIPS_R10000                         = 0x0168,                                                                                                                                                        // Colorize: green
-    MIPS_LITTLE_ENDIAN_WCI_V2           = 0x0169,                                                                                                                                                        // Colorize: green
-    OLD_ALPHA_AXP                       = 0x0183,                                                                                                                                                        // Colorize: green
-    ALPHA_AXP                           = 0x0184,                                                                                                                                                        // Colorize: green
-    HITACHI_SH3                         = 0x01A2,                                                                                                                                                        // Colorize: green
-    HITACHI_SH3_DSP                     = 0x01A3,                                                                                                                                                        // Colorize: green
-    HITACHI_SH4                         = 0x01A6,                                                                                                                                                        // Colorize: green
-    HITACHI_SH5                         = 0x01A8,                                                                                                                                                        // Colorize: green
-    ARM_LITTLE_ENDIAN                   = 0x01C0,                                                                                                                                                        // Colorize: green
-    THUMB                               = 0x01C2,                                                                                                                                                        // Colorize: green
-    ARMV7                               = 0x01C4,                                                                                                                                                        // Colorize: green
-    MATSUSHITA_AM33                     = 0x01D3,                                                                                                                                                        // Colorize: green
-    POWERPC_LITTLE_ENDIAN               = 0x01F0,                                                                                                                                                        // Colorize: green
-    POWERPC_WITH_FLOATING_POINT_SUPPORT = 0x01F1,                                                                                                                                                        // Colorize: green
-    INTEL_IA64                          = 0x0200,                                                                                                                                                        // Colorize: green
-    MIPS16                              = 0x0266,                                                                                                                                                        // Colorize: green
-    MOTOROLA_68000_SERIES               = 0x0268,                                                                                                                                                        // Colorize: green
-    ALPHA_AXP_64_BIT                    = 0x0284,                                                                                                                                                        // Colorize: green
-    MIPS_WITH_FPU                       = 0x0366,                                                                                                                                                        // Colorize: green
-    MIPS16_WITH_FPU                     = 0x0466,                                                                                                                                                        // Colorize: green
-    UEFI_BYTE_CODE                      = 0x0EBC,                                                                                                                                                        // Colorize: green
-    AMD_AMD64                           = 0x8664,                                                                                                                                                        // Colorize: green
-    MITSUBISHI_M32R_LITTLE_ENDIAN       = 0x9041,                                                                                                                                                        // Colorize: green
-    ARM64_LITTLE_ENDIAN                 = 0xAA64,                                                                                                                                                        // Colorize: green
-    CLR_PURE_MSIL                       = 0xC0EE                                                                                                                                                         // Colorize: green
-};                                                                                                                                                                                                       // Colorize: green
+{
+    UNKNOWN                             = 0,
+    MATSUSHITA_AM33                     = 0x01D3,
+    X64                                 = 0x8664,
+    ARM                                 = 0x01C0,
+    ARM64                               = 0xAA64,
+    ARMV7                               = 0x01C4,
+    UEFI_BYTE_CODE                      = 0x0EBC,
+    INTEL_386                           = 0x014C,
+    INTEL_IA64                          = 0x0200,
+    LOONG_ARCH32                        = 0x6232,
+    LOONG_ARCH64                        = 0x6264,
+    MITSUBISHI_M32R                     = 0x9041,
+    MIPS16                              = 0x0266,
+    MIPS_WITH_FPU                       = 0x0366,
+    MIPS16_WITH_FPU                     = 0x0466,
+    POWERPC                             = 0x01F0,
+    POWERPC_WITH_FLOATING_POINT_SUPPORT = 0x01F1,
+    MIPS_R4000                          = 0x0166,
+    RISC_V32                            = 0x5032,
+    RISC_V64                            = 0x5064,
+    RISC_V128                           = 0x5128,
+    HITACHI_SH3                         = 0x01A2,
+    HITACHI_SH3_DSP                     = 0x01A3,
+    HITACHI_SH4                         = 0x01A6,
+    HITACHI_SH5                         = 0x01A8,
+    THUMB                               = 0x01C2,
+    MIPS_WCE_V2                         = 0x0169,
+    MIPS_R3000                          = 0x0162,
+    MIPS_R10000                         = 0x0168,
+    OLD_ALPHA_AXP                       = 0x0183,
+    ALPHA_AXP                           = 0x0184,
+    ALPHA_AXP_64_BIT                    = 0x0284,
+    MOTOROLA_68000_SERIES               = 0x0268,
+    CLR_PURE_MSIL                       = 0xC0EE
+};
                                                                                                                                                                                                          // Colorize: green
                                                                                                                                                                                                          // Colorize: green
                                                                                                                                                                                                          // Colorize: green
